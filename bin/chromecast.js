6edb851736c7009cef7b484da3d8eb12536161f3
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-07-03 13:41:50 -0700

    added chromecast utility

--- bin/chromecast.js
+++ bin/chromecast.js
@@ -1,8 +1,26 @@
 #!/usr/bin/env node
 'use strict';
 
-var chromecasts = require('chromecasts')();
+var path = require('path'),
+    yargs = require('yargs'),
+    chromecasts = require('chromecasts')();
+
+var argv = yargs.argv;
+
+if (argv.url !== undefined && argv.device !== undefined) {
 
 chromecasts.on('update', function (player) {
-    console.log(player);
+    var url;
+    if (player.name === argv.device) {
+        url = argv.url;
+        player.play(
+            url,
+            {type: 'audio/x-mp3'},
+            function (err) {
+                console.log(err);
+            }
+        );
+    }
 });
+
+}
