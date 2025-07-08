9939747bd10ecd0791f1527e3a7f0b51783ae2b3
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2025-07-08 16:09:53 -0700

    started cleanup

--- bin/download.js
+++ bin/download.js
@@ -1,128 +1,133 @@
 #!/usr/bin/env node
 'use strict';
 
-var fs = require('fs'),
-    path = require('path'),
-    http = require('http'),
-    https = require('https'),
-    eachLimit = require('async/eachLimit'),
-    lib = require('../lib'),
-    chromecasts = require('chromecasts'),
-    yargs = require('yargs');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
-function getDest (i) {
-    return path.resolve(process.cwd(), './' + lib.getFileName(i));
-}
+const eachLimit = require('async/eachLimit');
+const chromecasts = require('chromecasts');
+const yargs = require('yargs');
 
-function download (index, cb) {
-    var url = lib.getUrl(index);
-    console.log(url);
-    if (url === undefined) {
-      cb();
-      return;
+const lib = require('../lib');
+
+function download ({src, dst}, cb) {
+  console.log(src);
+  if (src === undefined) {
+    cb();
+    return;
+  }
+  var type = src.slice(0, 5);
+  ({ 'http:': http, https: https })[type].get(src, function (response) {
+    process.stdout.write('start: ' + dst + '\n');
+    if (response.statusCode === 200) {
+      var file = fs.createWriteStream(dst);
+      response.pipe(file);
+      file.on('finish', function() {
+        file.close(cb);
+        process.stdout.write('done: ' + dst + '\n');
+      });
+    } else {
+      process.stdout.write('(' + response.statusCode + ')\n');
     }
-    var dest = getDest(index);
-    var type = url.slice(0, 5);
-    ({ 'http:': http, https: https })[type].get(url, function (response) {
-        process.stdout.write('start: ' + dest + '\n');
-        if (response.statusCode === 200) {
-            var file = fs.createWriteStream(dest);
-            response.pipe(file);
-            file.on('finish', function() {
-                file.close(cb);
-                process.stdout.write('done: ' + dest + '\n');
-            });
-        } else {
-            process.stdout.write('(' + response.statusCode + ')\n');
-        }
-    });
+  });
 }
 
 function getFiles(min, max, par) {
-    var arr = [];
-    var i;
-    for (i = min; i <= max; i++) {
-        arr.push(i);
-    }
-    eachLimit(arr, par, download, function (err) {
-        if (err) {
-            throw err;
-        }
-        if (i === max) {
-            console.log('all done');
-        } else {
-            console.log('done');
-            // i++;
-            // rec();
-        }
+  var arr = [];
+  var i;
+  for (i = min; i <= max; i++) {
+    arr.push({
+    //   src: lib.getArtUrl(i),
+    //   dst: lib.getArtDest(i)
+    // }, {
+      src: lib.getUrl(i),
+      dst: lib.getDest(i)
     });
+  }
+  eachLimit(arr, par, download, function (err) {
+    if (err) {
+      throw err;
+    }
+    if (i === max) {
+      console.log('all done');
+    } else {
+      console.log('done');
+      // i++;
+      // rec();
+    }
+  });
 }
 
-var argv = yargs
+const main = async () => {
+  var argv = yargs
     .usage('$0 [args]')
     .option('min', {
-        demand: false,
-        describe: 'first Aerostat number to download',
-        type: 'number'
+      demand: false,
+      describe: 'first Aerostat number to download',
+      type: 'number'
     })
     .option('max', {
-        demand: false,
-        default: 5000,
-        describe: 'last Aerostat number to download',
-        type: 'number'
+      demand: false,
+      default: 5000,
+      describe: 'last Aerostat number to download',
+      type: 'number'
     })
     .option('par', {
-        demand: false,
-        default: 1,
-        describe: 'number of parallel downloads',
-        type: 'number'
+      demand: false,
+      default: 1,
+      describe: 'number of parallel downloads',
+      type: 'number'
     })
     .option('number', {
-        alias: 'n',
-        demand: false,
-        describe: 'Aerostat Number to chromecast',
-        type: 'number'
+      alias: 'n',
+      demand: false,
+      describe: 'Aerostat Number to chromecast',
+      type: 'number'
     })
     .option('device', {
-        alias: 'd',
-        demand: false,
-        describe: 'Name of chromecast device',
-        type: 'string'
+      alias: 'd',
+      demand: false,
+      describe: 'Name of chromecast device',
+      type: 'string'
     })
     .options('skip', {
-        alias: 's',
-        describe: 'skip Number of seconds'
+      alias: 's',
+      describe: 'skip Number of seconds'
     })
     .help('help')
     .argv;
-
-if (argv.min !== undefined && argv.max !== undefined) {
+  
+  if (argv.min !== undefined && argv.max !== undefined) {
     getFiles(argv.min, argv.max, argv.par);
-}
-
-if (argv.number !== undefined && argv.device !== undefined) {
-
-chromecasts().on('update', function (player) {
-    var url;
-    console.log('found: ' + player.name);
-    if (player.name === argv.device) {
+  }
+  
+  if (argv.number !== undefined && argv.device !== undefined) {
+  
+    chromecasts().on('update', function (player) {
+      var url;
+      console.log('found: ' + player.name);
+      if (player.name === argv.device) {
         url = lib.getUrl(argv.number);
         player.play(
-            url,
-            {type: 'audio/x-mp3'},
-            function (err1) {
-                if (err1) { throw err1; }
-                if (argv.skip) {
-                    player.seek(
-                        Number(argv.skip),
-                        function (err2) {
-                            if (err2) { throw err2; }
-                        }
-                    );
+          url,
+          {type: 'audio/x-mp3'},
+          function (err1) {
+            if (err1) { throw err1; }
+            if (argv.skip) {
+              player.seek(
+                Number(argv.skip),
+                function (err2) {
+                  if (err2) { throw err2; }
                 }
+              );
             }
+          }
         );
-    }
-});
+      }
+    });
+  
+  }
+};
 
-}
+main();
